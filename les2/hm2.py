efab6fc6fab2e592b1489abf1d22dafbd3b093a4
vanyusha82228 <ivan00.02@mail.ru>
2022-12-20 17:36:10 +0300

    task 1

---
+++ les2/hm2.py
@@ -0,0 +1,19 @@
+# 1) Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# Пример:
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+n = int(input("input n: "))
+
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return fact(n-1)*n
+
+list = []
+
+for i in range(1,n+1):
+    list.append(fact(i))
+
+print(list)
