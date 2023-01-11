7fe8d78d376a2a79fbd30f44a27ce14f1043468a
vanyusha82228 <ivan00.02@mail.ru>
2023-01-11 14:06:52 +0300

    task 3

--- les3/hm3.py
+++ les3/hm3.py
@@ -25,17 +25,39 @@
 # - [2, 3, 5, 6] => [12, 15]
 
 
-list = [2, 3, 4, 5, 6]
-result = []
+# list = [2, 3, 4, 5, 6]
+# result = []
 
-len_list = len(list)
+# len_list = len(list)
 
-half_len= len_list/2
-if half_len %2 != 0:
-    half_len= int(half_len+1)
+# half_len= len_list/2
+# if half_len %2 != 0:
+#     half_len= int(half_len+1)
 
-for i in range(len_list):
-    if i< half_len:
-        result.append(list[i]*list[len_list-1-i])
+# for i in range(len_list):
+#     if i< half_len:
+#         result.append(list[i]*list[len_list-1-i])
 
-print(result)
+# print(result)
+
+
+
+
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+
+list = [1.1, 1.2, 3.1, 5, 10.01]
+
+min = 1
+max = 0
+
+for i in list:
+    if(i-int(i))<= min:
+        min = i - int(i)
+    if(i- int(i))>= max:
+        max = i - int(i)
+
+print(round(max-min, 2))
+
