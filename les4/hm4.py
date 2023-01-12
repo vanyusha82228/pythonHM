ac28089e06eaf1b5a63e2385842c912eabcabe18
vanyusha82228 <ivan00.02@mail.ru>
2023-01-12 16:35:41 +0300

    task 4

--- les4/hm4.py
+++ les4/hm4.py
@@ -35,17 +35,45 @@
 # 2 1
 
 
-import random
+# import random
 
-list= []
-for x in range(1, 10):
-    list.append(random.randint(1, 10))
+# list= []
+# for x in range(1, 10):
+#     list.append(random.randint(1, 10))
 
-print(list)
-my_list=[]
+# print(list)
+# my_list=[]
 
-for i in list:
-    if list.count(i) == 1:
-        my_list.append(i)
+# for i in list:
+#     if list.count(i) == 1:
+#         my_list.append(i)
 
-print(my_list)
+# print(my_list)
+
+
+
+
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и вывести многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 
+# k = 6
+#     ix^6 + ax^5 + bx^4+ cx^3 + dx^2 + ex + h
+#     a, b, c, d, e, h - рандом
+
+
+
+import random as rand
+
+
+def get_polynomial(pow):
+    string = ''
+
+    for i in range(pow, 1, -1):
+        string += str(rand.randint(1, 99)) + 'x^' + str(i) + ' + '
+
+    return string + str(rand.randint(1, 99)) + 'x + ' + str(rand.randint(1, 99))
+
+
+k = input('Введите степень: ')
+k = int(k)
+print(get_polynomial(k))
