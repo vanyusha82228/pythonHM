775fa08b0c01e8888dbfd11ae2eb8051d7a08786
vanyusha82228 <ivan00.02@mail.ru>
2023-01-12 16:00:48 +0300

    task 3

--- les4/hm4.py
+++ les4/hm4.py
@@ -12,16 +12,40 @@
 # 2 * 3 | 2 * 2 * 3 | 2 * 2 * 2 * 2 * 2 | 13 | 3 * 3 | 2 * 3 * 3 | 3*7
 
 
-n = int(input("Input n: "))
+# n = int(input("Input n: "))
 
-i = 2 
-list =[]
-while i*i<=n:
-    while n%i== 0:
-        list.append(i)
-        n=n/i
-    i+=1
-    if n>1:
-        list.append(n)
+# i = 2 
+# list =[]
+# while i*i<=n:
+#     while n%i== 0:
+#         list.append(i)
+#         n=n/i
+#     i+=1
+#     if n>1:
+#         list.append(n)
+
+# print(list)
+
+
+
+# Задайте последовательность чисел. Напишите программу, которая выведет список элементов, которые не имели повторов в исходной последовательности.
+# Ввод:
+# 3 1 2 3
+# Вывод:
+# 2 1
+
+
+import random
+
+list= []
+for x in range(1, 10):
+    list.append(random.randint(1, 10))
 
 print(list)
+my_list=[]
+
+for i in list:
+    if list.count(i) == 1:
+        my_list.append(i)
+
+print(my_list)
